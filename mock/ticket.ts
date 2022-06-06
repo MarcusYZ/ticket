a9e83b5bdc635ef8cce021ee6bb2eec3e7332698
TIANMAO - FIRST <40513247+MarcusYZ@users.noreply.github.com>
2022-06-06 16:22:42 +0800

    补全模拟数据

--- mock/ticket.ts
+++ mock/ticket.ts
@@ -16,18 +16,30 @@ const getTicketList = async (req: Request, res: Response) => {
       {
         avatarUrl: 'https://randomuser.me/api/portraits/men/19.jpg',
         info: '1号咖啡机损坏',
+        status: '待运维处理',
+        priority: '紧急',
+        time: '逾期 3 天 22 小时 44分',
       },
       {
         avatarUrl: 'https://randomuser.me/api/portraits/women/82.jpg',
         info: '2号咖啡机损坏',
+        status: '待运维处理',
+        priority: '高',
+        time: '逾期 3 天 22 小时 44分',
       },
       {
         avatarUrl: 'https://randomuser.me/api/portraits/women/40.jpg',
         info: '3号饮水机损坏',
+        status: '待运维处理',
+        priority: '中',
+        time: '逾期 3 天 22 小时 44分',
       },
       {
         avatarUrl: 'https://randomuser.me/api/portraits/women/40.jpg',
         info: '4号饮水机损坏',
+        status: '待运维补充材料',
+        priority: '低',
+        time: '逾期 3 天 22 小时 44分',
       },
     ],
   });
