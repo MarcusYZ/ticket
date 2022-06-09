599b8cfeb5c51cf382506daffcdd5297de976da9
TIANMAO - FIRST <40513247+MarcusYZ@users.noreply.github.com>
2022-06-09 17:51:25 +0800

    header 样式

--- src/pages/tickets/components/TicketHeader/index.tsx
+++ src/pages/tickets/components/TicketHeader/index.tsx
@@ -51,13 +51,15 @@ const TicketHeader: React.FC<TicketHeaderProps> = (props) => {
           </Col>
         </Row>
         {/* Tab页 */}
-        <Tabs defaultActiveKey="1" centered>
-          <TabPane tab="待我处理" key="1" />
-          <TabPane tab="我发起的" key="2" />
-          <TabPane tab="我参与的" key="3" />
-          <TabPane tab="全部" key="4" />
-          <TabPane tab="草稿" key="5" />
-        </Tabs>
+        <div className={`${styles.tabTitle} ${styles.tabShadow}`}>
+          <Tabs defaultActiveKey="1" centered>
+            <TabPane tab="待我处理" key="1" />
+            <TabPane tab="我发起的" key="2" />
+            <TabPane tab="我参与的" key="3" />
+            <TabPane tab="全部" key="4" />
+            <TabPane tab="草稿" key="5" />
+          </Tabs>
+        </div>
       </div>
     </div>
   );
