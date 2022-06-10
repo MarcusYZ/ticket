ca509165dceed2bb1c4f40f66c7319c5e7319c3e
TIANMAO - FIRST <40513247+MarcusYZ@users.noreply.github.com>
2022-06-10 08:10:07 +0800

    列表顶部控制按钮

--- src/pages/tickets/components/TopControl/index.tsx
+++ src/pages/tickets/components/TopControl/index.tsx
@@ -1,20 +1,20 @@
-import { ControlOutlined, ExpandOutlined, FunnelPlotOutlined } from '@ant-design/icons';
-import { Button, Col, Row } from 'antd';
+import { Col, Row } from 'antd';
+import deal from '../../../../../public/icons/deal.png';
+import filter from '../../../../../public/icons/filter.png';
+import operate from '../../../../../public/icons/operate.png';
+import styles from './index.less';
 
 // 顶部功能
 const TopControl = () => {
   return (
     <Row justify="end" style={{ marginTop: 42, marginBottom: 34, marginRight: 40 }}>
       <Col>
-        <Button type="text">
-          <ExpandOutlined />
-        </Button>
-        <Button type="text">
-          <ControlOutlined />
-        </Button>
-        <Button type="text">
-          <FunnelPlotOutlined />
-        </Button>
+        <img src={deal} alt="" className={styles.controlDeal} />
+        <img src={operate} alt="" className={styles.controlOperate} />
+        <div className={styles.controlFilter}>
+          <img src={filter} alt="" className={styles.controlIcon} />
+          <span className={styles.controlFilterText}>2项</span>
+        </div>
       </Col>
     </Row>
   );
